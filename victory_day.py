3a22227cd3b1a465d69803c140241d9e7854ac6f
altyn93 <altynannataganova2@gmail.com>
2025-05-08 16:57:32 +0500

    Create victory_day.py

---
+++ victory_day.py
@@ -0,0 +1,19 @@
+# victory_day.py
+import streamlit as st
+
+st.set_page_config(page_title="День Победы", page_icon="🎖", layout="centered")
+
+st.title("🎖 С Днём Победы!")
+st.subheader("9 Мая — День великой Победы!")
+
+st.markdown(
+    """
+    Спасибо за мужество, за стойкость и за мир!
+
+    🕊️ **Мы помним. Мы гордимся.**
+    """
+)
+
+st.image("https://www.istockphoto.com/photo/flowers-at-memorial-to-tomb-of-unknown-soldier-gm149042417-20458353?utm_source=pexels&utm_medium=affiliate&utm_campaign=sponsored_photo&utm_content=srp_inline_media&utm_term=%D0%B4%D0%B5%D0%BD%D1%8C%20%D0%BF%D0%BE%D0%B1%D0%B5%D0%B4%D1%8B")
+
+st.write("Создано с любовью ❤️ в честь 9 Мая")
